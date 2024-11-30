1c88e13a09b4976bf80c288fb79d55a7f7ed5c6e
imok1948 <imok1948@gmail.com>
2024-11-30 23:04:19 +0530

    Random colors are workingx

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,46 @@
 #include <Arduino.h>
-
 #include <Adafruit_NeoPixel.h>
+
 #define PIN 2        // input pin Neopixel is attached to
-#define NUMPIXELS 20 // number of neopixels in strip
+#define NUMPIXELS 68 // number of neopixels in strip
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int delayTime = 1;
-
-int redColor = 211;
-int greenColor = 3;
-int blueColor = 252;
-
-int i, j, k;
+int delayTime = 1; // Delay between color changes
 
 void setup()
 {
   Serial.begin(9600);
-  pixels.begin();
+  pixels.begin(); // Initialize the NeoPixel library
+  randomSeed(analogRead(0)); // Seed the random number generator using an analog pin
 }
 
 void loop()
 {
-  // Neopixel test all colors
+  Serial.println("Assigning Random Colors to LEDs");
 
-  Serial.println("Up");
-  for (i = 0; i < 256; i++)
+  // Generate a unique random color
+  int red = random(0, 256);
+  int green = random(0, 256);
+  int blue = random(0, 256);
+
+  // Print the selected color to Serial
+  Serial.print("Random Color: R=");
+  Serial.print(red);
+  Serial.print(", G=");
+  Serial.print(green);
+  Serial.print(", B=");
+  Serial.println(blue);
+
+  // Set all LEDs to the same random color
+  for (int pixel = 0; pixel < NUMPIXELS; pixel++)
   {
-    Serial.println(i);
-    pixels.setPixelColor(3, pixels.Color(i, 100, i));
-    pixels.show();
-    delay(delayTime);
+    pixels.setPixelColor(pixel, pixels.Color(red, green, blue));
   }
 
+  // Show the updated colors
+  pixels.show();
 
-  delay(200);
-
-
-  Serial.println("Down");
-  for (i = 255; i >= 0; i--)
-  {
-    Serial.println(i);
-    pixels.setPixelColor(3, pixels.Color(i, 100, i));
-    pixels.show();
-    delay(delayTime);
-  }
-
-  delay(700);
+  // Delay for effect
+  delay(1000);
 }
